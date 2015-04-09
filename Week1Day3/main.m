a058af1b58c6166debbf91afd532c9eb19e5e664
cat8dog <catherine.reyto@gmail.com>
2015-04-08 23:23:12 -0400

    Initial Commit

---
+++ Week1Day3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Week1Day3
+//
+//  Created by Catherine Reyto on 2015-04-08.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
